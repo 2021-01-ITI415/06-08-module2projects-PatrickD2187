cf02726a122f95b27dbe33963e8c06d4fa535b69
PatrickD2187 <60270720+PatrickD2187@users.noreply.github.com>
2021-03-10 12:41:37 -0500

    Completed (No gold Cards)

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -38,7 +38,40 @@ public class Prospector : MonoBehaviour
 	void Awake()
 	{
 		S = this;
+		SetUpUITexts();
 	}
+
+	void SetUpUITexts()
+	{
+		// Set up the HighScore UI Text 
+		GameObject go = GameObject.Find("HighScore");
+		if (go != null)
+		{
+			highScoreText = go.GetComponent<Text>();
+		}
+		int highScore = ScoreManager.HIGH_SCORE;
+		string hScore = "High Score: " + Utils.AddCommasToNumber(highScore);
+		go.GetComponent<Text>().text = hScore;
+		// Set up the UI Texts that show at the end of the round 
+		go = GameObject.Find("GameOver");
+		if (go != null)
+		{
+			gameOverText = go.GetComponent<Text>();
+		}
+		go = GameObject.Find("RoundResult");
+		if (go != null)
+		{
+			roundResultText = go.GetComponent<Text>();
+		}
+
+		ShowResultsUI(false);
+	}
+	void ShowResultsUI(bool show)
+    {
+		gameOverText.gameObject.SetActive(show);
+		roundResultText.gameObject.SetActive(show);
+
+    }
 
 	void Start()
 	{
@@ -298,14 +331,30 @@ public class Prospector : MonoBehaviour
 	// Called when the game is over. Simple for now, but expandable 
 	void GameOver(bool won)
 	{
+		int score = ScoreManager.SCORE;
+		if (fsRun != null) score += fsRun.score;
 		if (won)
 		{
+			gameOverText.text = "Round Over";
+			roundResultText.text = "You won this round! \n Round Score: " + score;
+			ShowResultsUI(true);
 			//print("Game Over. You won! :)");
 			ScoreManager.EVENT(eScoreEvent.gameWin);
 			FloatingScoreHandler(eScoreEvent.gameWin);
 		}
 		else
 		{
+			gameOverText.text = "Game Over";
+			if (ScoreManager.HIGH_SCORE <= score)
+			{
+				string str = "You got the high score!\nHigh score: " + score;
+				roundResultText.text = str;
+			}
+			else
+			{
+				roundResultText.text = "Your final score was: " + score;
+			}
+			ShowResultsUI(true);
 			//print("Game Over. You Lost. :(");
 			ScoreManager.EVENT(eScoreEvent.gameLoss);
 			FloatingScoreHandler(eScoreEvent.gameLoss);
